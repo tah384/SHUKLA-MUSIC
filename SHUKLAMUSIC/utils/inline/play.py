8e3b805e9d7079509a70a7c17a01a470b8b6640a
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-02 09:54:58 +0530

    Update play.py

--- SHUKLAMUSIC/utils/inline/play.py
+++ SHUKLAMUSIC/utils/inline/play.py
@@ -34,25 +34,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏"
+        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭INDIAN-CODERS"
     elif 10 < umm < 20:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨"
+        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭INDIAN-CODERSﮩ٨"
     elif 20 <= umm < 30:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ﮩ٨꯭ـﮩ"
+        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩINDIAN-CODERSـ꯭꯭꯭꯭ﮩ٨꯭ﮩ٨꯭ـﮩ"
     elif 30 <= umm < 40:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ﮩ٨꯭ـﮩ"
+        bar = "꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩINDIAN-CODERSـ꯭꯭꯭꯭ﮩ٨꯭ﮩ٨꯭ـﮩ"
     elif 40 <= umm < 50:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭"
+        bar = "꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـINDIAN-CODERSـ꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭"
     elif 50 <= umm < 60:
-        bar = "꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭"
+        bar = "꯭ﮩـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭ﮩINDIAN-CODERSـﮩ꯭ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ـ꯭꯭꯭꯭"
     elif 60 <= umm < 70:
-        bar = "꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
+        bar = "꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭INDIAN-CODERSﮩ٨ـ꯭꯭꯭꯭꯭ﮩ٨꯭ــﮩ꯭ﮩ٨꯭ـﮩ"
     elif 70 <= umm < 80:
-        bar = "꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
+        bar = "꯭ﮩ٨꯭INDIAN-CODERSـﮩـ꯭꯭꯭꯭ﮩ٨ـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
     elif 80 <= umm < 95:
-        bar = "ـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
+        bar = "ـ꯭꯭꯭꯭INDIAN-CODERSﮩ٨꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
     else:
-        bar = "ـ꯭꯭꯭꯭𝙏𝙃𝙀 𝘼𝙍𝙏𝙄𝙎𝙏ﮩ٨꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
+        bar = "ـ꯭꯭꯭꯭INDIAN-CODERSﮩ٨꯭ﮩ٨꯭ـﮩـ꯭꯭꯭꯭꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ꯭ﮩ٨꯭ـﮩ"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -61,10 +61,10 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="🇸▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🇴।। ", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🇳▷▷। ", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text=" G □ ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="I▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="N।। ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="D▷▷। ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" C □ ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
          InlineKeyboardButton(text="😈 𝐎𝐰𝐧𝐞𝐫 😈", user_id=config.OWNER_ID),
@@ -78,10 +78,10 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="🇸▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="🇴।। ", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="🇳▷▷। ", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text=" G □ ", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="I▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="N।। ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="D▷▷। ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" C □ ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
          InlineKeyboardButton(text="😈 𝐎𝐰𝐧𝐞𝐫 😈", user_id=config.OWNER_ID),
