720630e193f834072b813ec633f2b6865b362661
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-04 19:31:33 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -109,7 +109,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     welcome_font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=45)
     
     # Draw user's name with shining red fill and dark saffron border
-    draw.text((1770, 1015), f': {user}', fill=(255, 0, 0), font=font)
+    draw.text((1770, 1015), f': {user}', fill=(0, 0, 142), font=font)
     draw.text((1770, 1015), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=6)
     
     # Draw user's id with shining blue fill and white border
@@ -117,7 +117,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     draw.text((1530, 1230), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=0)
     
     # Draw user's username with white fill and green border
-    draw.text((2030, 1450), f': {uname}', fill=(255, 255, 255), font=font)
+    draw.text((2030, 1450), f': {uname}', fill=(0, 0, 145), font=font)
     draw.text((2030, 1450), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
     
     # Resize photo and position
