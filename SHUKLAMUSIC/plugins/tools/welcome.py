015172040f598052c8891c307aa06b31ba18a43e
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-07 07:52:34 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -88,9 +88,9 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
   #  draw.text((630, 300), f'NAME: {user}', fill=(234, 234, 234), font=font)
-   # draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((630, 450), f'ID: {id}', fill=(255, 255, 255), font=font)
 #    draw.text((630, 150), f"{chatname}", fill=(225, 225, 225), font=welcome_font)
-    draw.text((630, 230), f"USERNAME : {uname}", fill=(298, 298, 298), font=font)
+   # draw.text((630, 230), f"USERNAME : {uname}", fill=(298, 298, 298), font=font)
 
     #
     pfp_position = (48, 88)
