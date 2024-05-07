9ffcd994e5cb3b7755e481613a30d379e7227108
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-07 12:39:22 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -1,18 +1,29 @@
+#<<<<<<<<<<<<<<DiL>>>>>>>>>>>>>>#
+#<<<<<<<<<<<<<<Give<Credit<Else>You>Chutiya>>>>>>>>>>>>>>#
 import os
-from unidecode import unidecode
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from SHUKLAMUSIC import LOGGER
-from pyrogram.types import Message
-from SHUKLAMUSIC.misc import SUDOERS
-from SHUKLAMUSIC import app
-from SHUKLAMUSIC.CuteDb.Weldb import *
-from config import LOGGER_ID
+from AarohiX import app
 
 LOGGER = getLogger(__name__)
 
+class WelDatabase:
+    def __init__(self):
+        self.data = {}
+
+    async def find_one(self, chat_id):
+        return chat_id in self.data
+
+    async def add_wlcm(self, chat_id):
+        self.data[chat_id] = {}
+
+    async def rm_wlcm(self, chat_id):
+        if chat_id in self.data:
+            del self.data[chat_id]
+
+wlcm = WelDatabase()
 
 class temp:
     ME = None
@@ -22,78 +33,38 @@ class temp:
     U_NAME = None
     B_NAME = None
 
-def circle(pfp, size=(450, 450)):
-    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
+def circle(pfp, size=(500, 500)):
+    pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
     draw = ImageDraw.Draw(mask)
     draw.ellipse((0, 0) + bigsize, fill=255)
-    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = mask.resize(pfp.size, Image.LANCZOS)
     mask = ImageChops.darker(mask, pfp.split()[-1])
     pfp.putalpha(mask)
     return pfp
 
-def welcomepic(pic, user, chat, id, uname):
-    background = Image.open("SHUKLAMUSIC/assets/WELL2.PNG")
+
+def welcomepic(pic, user, chatname, id, uname):
+    background = Image.open("SHUKLAMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
-    pfp = pfp.resize(
-        (605, 605)
-    ) 
+    pfp = pfp.resize((1157, 1158))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=75)
-    font2 = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=90)
-    draw.text((150, 450), f'NAME : {unidecode(user)}', fill="black", font=font)
-    draw.text((150, 550), f'ID : {id}', fill="black", font=font)
-    draw.text((150, 650), f"USERNAME : {uname}", fill="black",font=font)
-    pfp_position = (1077, 183)  
-    background.paste(pfp, pfp_position, pfp)  
-    background.save(
-        f"downloads/welcome#{id}.png"
-    )
+    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
+    draw.text((1800, 700), f'NAME: {user}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 830), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((1800, 965), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
+    pfp_position = (391, 336)
+    background.paste(pfp, pfp_position, pfp)
+    background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
-
-
-HUHU = """**
-@app.on_message(filters.command("swel") & ~filters.private)
-async def auto_state(_, message):
-    usage = "**❖ ᴜsᴀɢᴇ ➥** /swel [ᴇɴᴀʙʟᴇ|ᴅɪsᴀʙʟᴇ]"
-    if len(message.command) == 1:
-        return await message.reply_text(usage)
-    chat_id = message.chat.id
-    user = await app.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status in (
-        enums.ChatMemberStatus.ADMINISTRATOR,
-        enums.ChatMemberStatus.OWNER,
-    ):
-      A = await wlcm.find_one({"chat_id" : chat_id})
-      state = message.text.split(None, 1)[1].strip()
-      state = state.lower()
-      if state == "enable":
-        if A:
-           return await message.reply_text("✦ Special Welcome Already Enabled")
-        elif not A:
-           await add_wlcm(chat_id)
-           await message.reply_text(f"✦ Enabled Special Welcome in {message.chat.title}")
-      elif state == "disable":
-        if not A:
-           return await message.reply_text("✦ Special Welcome Already Disabled")
-        elif A:
-           await rm_wlcm(chat_id)
-           await message.reply_text(f"✦ Disabled Special Welcome in {message.chat.title}")
-      else:
-        await message.reply_text(usage)
-    else:
-        await message.reply("✦ Only Admins Can Use This Command")
-  **  """
-#bhag 
 
 @app.on_chat_member_updated(filters.group, group=-3)
 async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-   # A = await wlcm.find_one({"chat_id" : chat_id})
-   # if not A:
-  #     return
+    A = await wlcm.find_one(chat_id)
     if (
         not member.new_chat_member
         or member.new_chat_member.status in {"banned", "left", "restricted"}
@@ -106,7 +77,7 @@ async def greet_group(_, member: ChatMemberUpdated):
             user.photo.big_file_id, file_name=f"pp{user.id}.png"
         )
     except AttributeError:
-        pic = "SHUKLAMUSIC/assets/upic.png"
+        pic = "SHUKLAMUSIC/assets/wel2.png"
     if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
         try:
             await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
@@ -119,24 +90,16 @@ async def greet_group(_, member: ChatMemberUpdated):
         temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
             member.chat.id,
             photo=welcomeimg,
-            caption= f"""
-ㅤㅤㅤ◦•●◉✿ ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ✿◉●•◦
-▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
-
-● ɴᴀᴍᴇ ➥  {user.mention}
-● ᴜsᴇʀɴᴀᴍᴇ ➥  @{user.username}
-● ᴜsᴇʀ ɪᴅ ➥  {user.id}
-
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ‌ ๛ᴍ ᴀ ʜ ᴀ ᴋ ♡゙
-▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
+            caption=f"""
+Wᴇʟᴄᴏᴍᴇ Tᴏ {member.chat.title}
+➖➖➖➖➖➖➖➖➖➖➖➖
+Nᴀᴍᴇ ✧ {user.mention}
+Iᴅ ✧ {user.id}
+Usᴇʀɴᴀᴍᴇ ✧ @{user.username}
+➖➖➖➖➖➖➖➖➖➖➖➖
 """,
-reply_markup=InlineKeyboardMarkup(
-[
-[InlineKeyboardButton(f"ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/mahakxbot?startgroup=new"),
-]
-]
-))
-
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ ᴀᴅᴅ ᴍᴇ ⦿", url=f"https://t.me/DevilXmusicrobot?startgroup=true")]])
+        )
     except Exception as e:
         LOGGER.error(e)
     try:
@@ -145,5 +108,15 @@ reply_markup=InlineKeyboardMarkup(
     except Exception as e:
         pass
 
-
-  
+@app.on_message(filters.new_chat_members & filters.group, group=-1)
+async def bot_wel(_, message):
+    for u in message.new_chat_members:
+        if u.id == app.me.id:
+            await app.send_message(LOG_CHANNEL_ID, f"""
+NEW GROUP
+➖➖➖➖➖➖➖➖➖➖➖➖
+NAME: {message.chat.title}
+ID: {message.chat.id}
+USERNAME: @{message.chat.username}
+➖➖➖➖➖➖➖➖➖➖➖➖
+""")
