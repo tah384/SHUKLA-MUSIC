06114c188bb6335bc4b1c8318733a9b28ed4920d
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2024-05-13 17:26:11 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -34,19 +34,19 @@ def circle(pfp, size=(450, 450)):
     return pfp
 
 def welcomepic(pic, user, chat, id, uname):
-    background = Image.open("AnonXMusic/assets/WELL2.PNG")
+    background = Image.open("SHUKLAMUSIC/assets/WELL2.PNG")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize(
-        (605, 605)
+        (450, 450)
     ) 
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('AnonXMusic/assets/font.ttf', size=65)
-    font2 = ImageFont.truetype('AnonXMusic/assets/font.ttf', size=90)
-    draw.text((150, 450), f'NAME : {unidecode(user)}', fill="black", font=font)
-    draw.text((150, 550), f'ID : {id}', fill="black", font=font)
-    draw.text((150, 650), f"USERNAME : {uname}", fill="black",font=font)
-    pfp_position = (1077, 183)  
+    font = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=60)
+    font2 = ImageFont.truetype('SHUKLAMUSIC/assets/font.ttf', size=90)
+    draw.text((65, 250), f'NAME : {unidecode(user)}', fill="black", font=font)
+    draw.text((65, 340), f'ID : {id}', fill="black", font=font)
+    draw.text((65, 430), f"USERNAME : {uname}", fill="black",font=font)
+    pfp_position = (767, 133)  
     background.paste(pfp, pfp_position, pfp)  
     background.save(
         f"downloads/welcome#{id}.png"
