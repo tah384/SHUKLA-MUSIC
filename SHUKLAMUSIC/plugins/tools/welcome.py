f094b7b05f6805523bad52ec9ee202e0d6b7cac2
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-16 12:18:49 +0530

    Update welcome.py

--- SHUKLAMUSIC/plugins/tools/welcome.py
+++ SHUKLAMUSIC/plugins/tools/welcome.py
@@ -34,7 +34,7 @@ def circle(pfp, size=(450, 450)):
     return pfp
 
 def welcomepic(pic, user, chat, id, uname):
-    background = Image.open("SHUKLAMUSIC/assets/WELL2.PNG")
+    background = Image.open("SHUKLAMUSIC/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize(
