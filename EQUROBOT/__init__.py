ffdc15af9a810286313c1050e7f0b2807ef49fb3
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-24 08:22:58 +0530

    Add files via upload

---
+++ EQUROBOT/__init__.py
@@ -0,0 +1,50 @@
+import asyncio
+import logging
+import time
+from importlib import import_module
+from os import listdir, path
+from dotenv import load_dotenv
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME, OWNER_ID, GPT_API, LOGGER_ID, DEEP_API
+
+from SafoneAPI import SafoneAPI
+
+safone = SafoneAPI()
+
+
+loop = asyncio.get_event_loop()
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+)
+
+
+
+app = Client(
+    ":EQUROBOT:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+
+
+
+
+
+
+
+async def info_bot():
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    await app.start()
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+
+
+loop.run_until_complete(info_bot())
