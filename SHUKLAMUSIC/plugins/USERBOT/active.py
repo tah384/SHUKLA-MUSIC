089358c9456b4e3a4e9339dd54b7abdfa1c2d348
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-29 10:14:04 +0530

    Update active.py

--- SHUKLAMUSIC/plugins/USERBOT/active.py
+++ SHUKLAMUSIC/plugins/USERBOT/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from unidecode import unidecode
 
-from VIPMUSIC import app
-from VIPMUSIC.misc import SUDOERS
-from VIPMUSIC.utils.database import (
+from SHUKLAMUSIC import app
+from SHUKLAMUSIC.misc import SUDOERS
+from SHUKLAMUSIC.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
