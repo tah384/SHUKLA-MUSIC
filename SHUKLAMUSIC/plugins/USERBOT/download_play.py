726d3ee2ad4c03eecd3ca21d88ad638e231779f2
INDIAN CODER <167271009+INDIANCODERS-1@users.noreply.github.com>
2024-05-29 10:25:19 +0530

    Update download_play.py

--- SHUKLAMUSIC/plugins/USERBOT/download_play.py
+++ SHUKLAMUSIC/plugins/USERBOT/download_play.py
@@ -15,7 +15,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from VIPMUSIC import app
+from SHUKLAMUSIC import app
 import asyncio
 import os
 import time
@@ -25,10 +25,10 @@ from pyrogram import filters
 from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from VIPMUSIC import app
+from SHUKLAMUSIC import app
 from time import time
 import asyncio
-from VIPMUSIC.utils.extraction import extract_user
+from SHUKLAMUSIC.utils.extraction import extract_user
 import asyncio
 import os
 import wget
@@ -37,16 +37,16 @@ from pyrogram.types import Message
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
 
-from VIPMUSIC import app
-from VIPMUSIC.utils.extraction import extract_user
+from SHUKLAMUSIC import app
+from SHUKLAMUSIC.utils.extraction import extract_user
 from time import time
-from VIPMUSIC.utils.extraction import extract_user
+from SHUKLAMUSIC.utils.extraction import extract_user
 from urllib.parse import urlparse
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters, Client
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-from VIPMUSIC import app
+from SHUKLAMUSIC import app
 import wget
 
 # Define a dictionary to track the last query timestamp for each user
